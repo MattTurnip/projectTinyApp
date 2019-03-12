1dfbba18c05b50b479b26a22389bfa43c21790e2
MattTurnip <mattadavey@gmail.com>
2019-03-12 17:54:01 +0000

    added another route to irls_show.ejs

--- express_server.js
+++ express_server.js
@@ -22,9 +22,16 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//Sending Data to urls_index.ejs
 app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
+});
+
+//Adding a Second Route to url_show.ejs and Template
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
