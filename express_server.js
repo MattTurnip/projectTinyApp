c9f51aee83eafbbc72ae9f59b8b4a60de7568990
MattTurnip <mattadavey@gmail.com>
2019-03-12 18:58:10 +0000

    require body-parse and initialize use ... of ... it

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 app.set("view engine", "ejs"); //set ejs as the view engine
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
