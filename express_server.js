916652cbaa4013b019dbdd6605cadf5d3e108434
MattTurnip <mattadavey@gmail.com>
2019-03-12 16:21:11 +0000

    added ejs as view engine

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+app.set("view engine", "ejs"); //set ejs as the view engine
+
+
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
