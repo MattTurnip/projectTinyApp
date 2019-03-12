08ba0ddf4a24b5781c5f942386f3b257c3d211a7
MattTurnip <mattadavey@gmail.com>
2019-03-12 20:20:44 +0000

    added dummy post route and empty generate random numbers function

--- express_server.js
+++ express_server.js
@@ -35,6 +35,11 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 //Adding a rout to url_show.ejs and Template
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
