590f0491fadc4484fdb08023606b6fd07e511f21
MattTurnip <mattadavey@gmail.com>
2019-03-15 21:42:32 +0000

    styled templates

--- express_server.js
+++ express_server.js
@@ -15,18 +15,7 @@ app.use(cookieSession({
 
 const urlDatabase = {};
 
-const userStore = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
+const userStore = {};
 
 //Get Requests
 
